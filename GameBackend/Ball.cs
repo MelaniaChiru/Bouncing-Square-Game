9a7fb12b64cc5690753592f8585124d22194cb2a
Melania Chiru <melania.chiru@dawsoncollege.qc.ca>
2025-08-26 09:33:27 -0400

    define Ball class

---
+++ GameBackend/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBackend
+{
+    internal class Ball
+    {
+        private Coordinate _coordinate;
+
+        public Coordinate Coordinate
+        {
+            get { return _coordinate; }
+            set { _coordinate = value; }
+        }
+
+        public Ball(int x, int y)
+        {
+            _coordinate = new Coordinate(x, y);
+        }
+
+        public Move()
+        {
+            float oldX = _coordinate.x;
+            float oldY = _coordinate.y;
+
+            float newX = oldX + 1;
+            float newY = oldY + 1;
+
+            _coordinate = new Coordinate(newX, newY);
+        }
+    }
+}
