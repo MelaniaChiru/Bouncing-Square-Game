7b65c8dfbf793a09d50baf9694617913bed4b55e
Melania Chiru <melania@chiru.ca>
2025-08-28 14:07:19 -0400

    add necessary files back

---
+++ GameBackendTests/BallTests.cs
@@ -0,0 +1,72 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using GameBackend;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace GameBackend.Tests
+//{
+//    [TestClass()]
+//    public class BallTests
+//    {
+//        [TestMethod()]
+//        public void BallTest_ValidInputs()
+//        {
+//            // Arrange
+//            float expectedXCoordinate = 55;
+//            float expectedYCoordinate = 12;
+
+//            // Act
+//            Ball ball = new Ball(expectedXCoordinate, expectedYCoordinate);
+//            float actualXCoordinate = ball.Coordinate.X;
+//            float actualYCoordinate = ball.Coordinate.Y;
+
+//            //Assert
+//            Assert.AreEqual(expectedXCoordinate, actualXCoordinate);
+//            Assert.AreEqual(expectedYCoordinate, actualYCoordinate);
+//        }
+
+//        [TestMethod()]
+//        public void BallTest_InvalidInputs()
+//        {
+//            // Arrange
+//            float invalidXCoordinate = -55;
+//            float validYCoordinate = 12;
+
+//            // Act
+//            try
+//            {
+//                Ball ball = new Ball(invalidXCoordinate, validYCoordinate);
+
+//                // Assert
+//                Assert.Fail();
+//            }
+//            catch (ArgumentException e) 
+//            {
+//            }
+//        }
+
+//        [TestMethod()]
+//        public void MoveTest()
+//        {
+//            // Arrange
+//            float xCoordinate = 50;
+//            float yCoordinate = 50;
+//            Ball ball = new Ball(xCoordinate, yCoordinate);
+
+//            float expectedXCoordinate = xCoordinate + 1;
+//            float expectedYCoordinate = yCoordinate + 1;
+
+//            // Act
+//            ball.Move();
+//            float actualXCoordinate = ball.Coordinate.X;
+//            float actualYCoordinate = ball.Coordinate.Y;
+
+//            //Assert
+//            Assert.AreEqual(expectedXCoordinate, actualXCoordinate);
+//            Assert.AreEqual(expectedYCoordinate, actualYCoordinate);
+//        }
+//    }
+//}
---
+++ GameBackendTests/CoordinateTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameBackend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBackend.Tests
+{
+    [TestClass()]
+    public class CoordinateTests
+    {
+        [TestMethod()]
+        public void CoordinateTest_ValidInputs()
+        {
+            // Arrange
+            float expectedXCoordinate = 10;
+            float expectedYCoordinate = 58;
+
+            // Act
+            Coordinate coordinate = new Coordinate(expectedXCoordinate, expectedYCoordinate);
+            float actualXCoordinate = coordinate.X;
+            float actualYCoordinate = coordinate.Y;
+
+            // Assert
+            Assert.AreEqual(expectedXCoordinate, actualXCoordinate);
+            Assert.AreEqual(expectedYCoordinate, actualYCoordinate);
+        }
+
+        [TestMethod()]
+        public void CoordinateTest_InvalidXCoordinate()
+        {
+            // Arrange
+            float invalidXCoordinate = -10;
+            float validYCoordinate = 58;
+
+            // Act
+            try
+            {
+                Coordinate coordinate = new Coordinate(invalidXCoordinate, validYCoordinate);
+                // If error is not thrown, an error has occured
+                // Assert
+                Assert.Fail();
+            } catch (ArgumentException e)
+            {
+            }
+        }
+
+        [TestMethod()]
+        public void CoordinateTest_InvalidYCoordinate()
+        {
+            // Arrange
+            float validXCoordinate = 10;
+            float invalidYCoordinate = -58;
+
+            // Act
+            try
+            {
+                Coordinate coordinate = new Coordinate(validXCoordinate, invalidYCoordinate);
+                // If error is not thrown, an error has occured
+                // Assert
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+            }
+        }
+    }
+}
